d72dcbcbb8cb5261f9fe8a222340c5528fe39b8e
Viktor Gurev <siplyjt@gmail.com>
2025-02-28 20:10:04 +0100

    Automatic generation of PDF price lists using jsPDF has been added

--- src/components/PriceLayout.js
+++ src/components/PriceLayout.js
@@ -1,19 +1,13 @@
-import { Link } from 'react-router-dom';
-
 import PriceTable from './PriceTable';
+import GeneratePricePDF from './GeneratePricePDF';
 import { useMainNavigation } from './Contexts/MainNavigationContext';
 
 import classes from './PriceLayout.module.css';
 import { useTranslation } from 'react-i18next';
 
 function PriceLayout() {
-	const { t, i18n } = useTranslation();
+	const { t } = useTranslation();
 	const { mainNavigationHeight } = useMainNavigation();
-
-	let priceFileUrl = `https://emptywall.design/files/price/price_${i18n.language}.pdf`;
-	if (i18n.language === 'uk') {
-		priceFileUrl = 'https://emptywall.design/files/price/price_ua.pdf';
-	};
 	
 	return (
 		<div
@@ -22,22 +16,26 @@ function PriceLayout() {
 		>
 			<h1 className="block_title">{t('price.title')}</h1>
 			<span className="block_description">{t('price.description')}</span>
-
-			<PriceTable />
 			
-			<div className={classes.price_notes} >
-				<span className={classes.price_note} >{t('price.note')}</span>
+			<div id="price_data">
+				<PriceTable />
+			
+				<div className={classes.price_notes} >
+					<span className={classes.price_note} >{t('price.note')}</span>
 
-				<h3 className={classes.price_additions_title}>{t('price.additions_title')}</h3>
+					<h3 className={classes.price_additions_title}>{t('price.additions_title')}</h3>
 
-				<ul className={classes.price_additions_list} >
-					<li>{t('price.addition_1')}</li>
-					<li>{t('price.addition_2')}</li>
-				</ul>
+					<ul className={classes.price_additions_list} >
+						<li>{t('price.addition_1')}</li>
+						<li>{t('price.addition_2')}</li>
+					</ul>
 
-				<Link className={classes.price_pdf} to={priceFileUrl} target="_blank" download>
-					<input className={classes.price_pdf_button} type="button" value={t('price.pdf_button')} />
-				</Link>
+					{/* Real-time PDF generation button */}
+					<button className={classes.price_pdf_button} onClick={() => GeneratePricePDF(classes)}>
+						{t("price.pdf_button")}
+					</button>
+				</div>
+
 			</div>
 		</div>
 	);
